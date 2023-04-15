70ddf9ce07383772cdeec9b9453aa264de525254
usinia <usinia0103@gmail.com>
2023-04-15 17:27:52 +0900

    1.8 compiler

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("hello world!!!");
+var Car = /** @class */ (function () {
+    function Car() {
+        this.engine = 1;
+    }
+    return Car;
+}());
+// npm install -g ts-node
+// tsc -h
+// tsc --watch
---
+++ main.ts
@@ -0,0 +1,12 @@
+console.log("hello world!!!");
+
+class Car {
+  engine: number;
+  constructor() {
+    this.engine = 1;
+  }
+}
+
+// npm install -g ts-node
+// tsc -h
+// tsc --watch
