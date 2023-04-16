d4f34dbabef899191334718e807aa67b68a0bf52
usinia <usinia0103@gmail.com>
2023-04-16 13:54:01 +0900

    [#3] 4.9

---
+++ ch4/3-3-encapsulation.ts
@@ -0,0 +1,41 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: false;
+  };
+
+  class CoffeeMaker {
+    private static BEANS_CRAMM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
+
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_CRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_CRAMM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const maker = CoffeeMaker.makeMachine(32);
+  maker.fillCoffeeBeans(14);
+  console.log(maker);
+}
