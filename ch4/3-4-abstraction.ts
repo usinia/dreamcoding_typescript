fa786082d92ba9e0c5b886addf1b4ed3fb593ce5
usinia <usinia0103@gmail.com>
2023-04-16 14:30:01 +0900

    [#3] 4.11

---
+++ ch4/3-4-abstraction.ts
@@ -0,0 +1,63 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: false;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_CRAMM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
+
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_CRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots & CoffeeMachine.BEANS_CRAMM_PER_SHOT;
+    }
+    private preheat(): void {
+      console.log("heating up... 🔥");
+    }
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots... ☕️`);
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  maker.fillCoffeeBeans(14);
+  maker.makeCoffee(2);
+  console.log(maker);
+
+  const maker2: CoffeeMaker = CoffeeMachine.makeMachine(32);
+  //  maker2.fillCoffeeBeans(14); // interface 에 선언되어 있지 않기 때문에 사용할 수 없음.
+  maker2.makeCoffee(2);
+  console.log(maker2);
+}
