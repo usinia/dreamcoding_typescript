2c190c5fa31b6178afb2dee140c8199bf38da39b
usinia <usinia0103@gmail.com>
2023-04-16 14:02:25 +0900

    [#3] 4.10

--- ch4/3-3-encapsulation.ts
+++ ch4/3-3-encapsulation.ts
@@ -38,4 +38,43 @@
   const maker = CoffeeMaker.makeMachine(32);
   maker.fillCoffeeBeans(14);
   console.log(maker);
+
+  // 4.10 getter & setter
+  class User {
+    /*
+    firstName: string;
+    lastName: string;
+    // fullName: string;
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      //   this.fullName = `${firstName} ${lastName}`;
+    }
+    */
+    constructor(private firstName: string, private lastName: string) {}
+    // 자동으로 private firstName 이 선언되고 값도 할당 됨
+
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+
+    private internalAge = 4;
+    get age(): number {
+      return this.internalAge;
+    }
+    set age(num: number) {
+      if (num > 0) {
+        // setter 에서 값 유효성 검사 로직도 추가할 수 있음
+        this.internalAge = num;
+      }
+    }
+  }
+
+  const user = new User("Steve", "Jobs");
+  console.log(user);
+  //   user.firstName = "usinia";
+  console.log(user.fullName);
+
+  user.age = 32;
+  console.log(user);
 }
