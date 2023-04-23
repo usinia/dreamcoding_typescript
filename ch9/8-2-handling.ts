8a29547863df9f2ee624c8df688080f13e712929
usinia <usinia0103@gmail.com>
2023-04-23 15:10:56 +0900

    [#6] 9.4

---
+++ ch9/8-2-handling.ts
@@ -0,0 +1,39 @@
+{
+  class NetworkClient {
+    tryConnect(): void {
+      throw new Error("no network!!!");
+    }
+  }
+
+  class UserService {
+    constructor(private client: NetworkClient) {}
+    login() {
+      this.client.tryConnect(); // error occurred
+      // errorr 가 발생했을 때 무언가 처리할 수 있는게 아니라면 (어정쩡하게 catch 하는 것보다) catch를 하지 않는 것이 더 낫다.
+      // login...
+    }
+  }
+
+  //   const client = new NetworkClient();
+  //   const service = new UserService(client);
+  //   service.login();
+
+  class App {
+    constructor(private userService: UserService) {}
+    run() {
+      // application level 에서 처리하는 것이 더 좋다.
+      try {
+        this.userService.login();
+      } catch (error) {
+        //show dialog to user
+        console.log("error occurred...");
+        console.log("show dialog to user");
+      }
+    }
+  }
+
+  const client = new NetworkClient();
+  const service = new UserService(client);
+  const app = new App(service);
+  app.run();
+}
