ecea3deca238c98b25ecd25ab4b2f04e7a925dd8
usinia <usinia0103@gmail.com>
2023-05-02 16:09:09 +0900

    [#11] 14.23

--- ch14/src/app.ts
+++ ch14/src/app.ts
@@ -1,3 +1,4 @@
+import { InputDialog } from "./components/dialog/dialog.js";
 import { VideoComponent } from "./components/page/item/video.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { NoteComponent } from "./components/page/item/note.js";
@@ -36,6 +37,21 @@ class App {
     const todo = new TodoComponent("todo", "must todo");
     // todo.attachTo(appRoot, "beforeend");
     this.page.addChild(todo);
+
+    // button dialog
+    const imageBtn = document.querySelector("#new-image")! as HTMLButtonElement;
+    imageBtn.addEventListener("click", () => {
+      const dialog = new InputDialog();
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      });
+      dialog.setOnSubmitListener(() => {
+        // 섹션을 만들어서 페이지에 추가
+        dialog.removeFrom(document.body);
+      });
+
+      dialog.attachTo(document.body);
+    });
   }
 }
 
---
+++ ch14/src/components/dialog/dialog.ts
@@ -0,0 +1,46 @@
+import { Composable } from "./../page/page.js";
+import { BaseComponent, Component } from "../component.js";
+
+type OnCloseListener = () => void;
+type OnSubmitListener = () => void;
+export class InputDialog
+  extends BaseComponent<HTMLElement>
+  implements Composable
+{
+  closeListener?: OnCloseListener;
+  submitListener?: OnSubmitListener;
+  constructor() {
+    super(`<dialog class="dialog">
+             <div class="dialog__container">
+               <button class="close">&times;</button>
+               <div id="dialog__body"></div>
+               <button class="dialog__submit">ADD</button>
+             </div>
+           </dialog>`);
+    const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
+    const submitBtn = this.element.querySelector(
+      ".dialog__submit"
+    )! as HTMLButtonElement;
+
+    // closeBtn.addEventListener("click", () => {});    // event 를 여러 개 등록 가능
+    closeBtn.onclick = () => {
+      // click event 를 덮어씀
+      this.closeListener && this.closeListener();
+    };
+    submitBtn.onclick = () => {
+      this.submitListener && this.submitListener();
+    };
+  }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
+  }
+  setOnSubmitListener(listener: OnSubmitListener) {
+    this.submitListener = listener;
+  }
+
+  addChild(child: Component): void {
+    const body = this.element.querySelector("#dialog__body")! as HTMLElement;
+    child.attachTo(body);
+  }
+}
