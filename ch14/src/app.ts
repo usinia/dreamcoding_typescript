c1507e17d303affbee4d4c9a1e3b100e0bad54b6
usinia <usinia0103@gmail.com>
2023-05-02 17:00:53 +0900

    [#11] 14.27

--- ch14/src/app.ts
+++ ch14/src/app.ts
@@ -1,4 +1,8 @@
-import { InputDialog } from "./components/dialog/dialog.js";
+import {
+  InputDialog,
+  MediaData,
+  TextData,
+} from "./components/dialog/dialog.js";
 import { VideoComponent } from "./components/page/item/video.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { NoteComponent } from "./components/page/item/note.js";
@@ -12,9 +16,12 @@ import { Component } from "./components/component.js";
 import { MediaSectionInput } from "./components/dialog/input/media-input.js";
 import { TextSectionInput } from "./components/dialog/input/text-input.js";
 
+type InputComponentConstructor<T = (MediaData | TextData) & Component> = {
+  new (): T;
+};
 class App {
   private readonly page: Component & Composable;
-  constructor(appRoot: HTMLElement, root: HTMLElement) {
+  constructor(appRoot: HTMLElement, private root: HTMLElement) {
     // --- 1. 페이지 생성
     this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
@@ -42,6 +49,7 @@ class App {
     // // todo.attachTo(appRoot, "beforeend");
     // this.page.addChild(todo);
 
+    /*
     // --- 3. 버튼 다이어로그 생성
     // button dialog
     const imageBtn = document.querySelector("#new-image")! as HTMLButtonElement;
@@ -117,6 +125,52 @@ class App {
         const todo = new TodoComponent(inputSection.title, inputSection.body);
         this.page.addChild(todo);
         dialog.removeFrom(root);
+      });
+    });
+    */
+
+    this.bindElementToDialog<MediaSectionInput>(
+      "#new-image",
+      MediaSectionInput,
+      (input: MediaSectionInput) => new ImageComponent(input.title, input.url)
+    );
+    this.bindElementToDialog<MediaSectionInput>(
+      "#new-vedio",
+      MediaSectionInput,
+      (input: MediaSectionInput) => new VideoComponent(input.title, input.url)
+    );
+    this.bindElementToDialog<TextSectionInput>(
+      "#new-note",
+      TextSectionInput,
+      (input: TextSectionInput) => new NoteComponent(input.title, input.body)
+    );
+    this.bindElementToDialog<TextSectionInput>(
+      "#new-todo",
+      TextSectionInput,
+      (input: TextSectionInput) => new TodoComponent(input.title, input.body)
+    );
+  }
+
+  private bindElementToDialog<T extends (MediaData | TextData) & Component>(
+    selector: string,
+    InputComponent: InputComponentConstructor<T>,
+    makeSection: (input: T) => Component
+  ) {
+    const btn = document.querySelector(selector)! as HTMLButtonElement;
+    btn.addEventListener("click", () => {
+      const dialog = new InputDialog();
+      const input = new InputComponent();
+
+      dialog.addChild(input);
+      dialog.attachTo(this.root);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(this.root);
+      });
+      dialog.setOnSubmitListener(() => {
+        const item = makeSection(input);
+        this.page.addChild(item);
+        dialog.removeFrom(this.root);
       });
     });
   }
